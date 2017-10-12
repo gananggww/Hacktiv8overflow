17464ef1c94092d7113f1c33fd05b80a2c050d17
gananggww <gananggww@gmail.com>
2017-10-12 22:58:23 +0700

    update atlas db

--- server/app.js
+++ server/app.js
@@ -3,15 +3,18 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/hacktiv8overflow', (err)=>{
+mongoose.connect('mongodb://gananggww:UyxNtLEd4lwSaY5l@cluster0-shard-00-00-rcykk.mongodb.net:27017,cluster0-shard-00-01-rcykk.mongodb.net:27017,cluster0-shard-00-02-rcykk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err)=>{
   if(err){
     console.log(err);
   }else{
     console.log("database conected");
   }
 });
-app.use(cors())
 
+
+// UyxNtLEd4lwSaY5l
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
